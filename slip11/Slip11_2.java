34dbb563d9e227a5552b2b0cb29e8e0af43aa72f
prohackercs05 <prohackercs03@gmail.com>
2026-03-01 21:04:34 +0530

    Change database user from 'root' to 'postgres'

--- slip11/Slip11_2.java
+++ slip11/Slip11_2.java
@@ -4,7 +4,7 @@ public class Slip11_2 {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "postgres", "root");
 
             Statement st = con.createStatement();
 
